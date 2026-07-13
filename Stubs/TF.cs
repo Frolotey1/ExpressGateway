3475c9069c949ae28891f35256dd895dc7b26f41
fr1zb3e <frolotey@gmail.com>
2026-07-13 12:45:29 +0500

    Добавление XUnitTests и Meteolib файлов для проекта

---
+++ Stubs/TF.cs
@@ -0,0 +1,12 @@
+﻿using MeteoLib.Impl.Repo;
+using MeteoLib.Interfaces;
+
+namespace Stubs;
+
+internal class TF
+{
+    public IMeteoAPI Api { get; } = new TestApi();
+    public IRepo Repo { get; } = new MemoRepo();
+
+    internal static IConnectionFactory MegaConnection() => new MegatestConnectionFactory();
+}
---
+++ xUnitTests/FormatterTests.cs
@@ -0,0 +1,35 @@
+﻿using MeteoLib;
+using MeteoLib.Impl.Delivery;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace xUnitTests
+{
+    public class FormatterTests
+    {
+        [Fact]
+        public void Escapes_0()
+        {
+            var f = new TelegramFormatter();
+
+            var hello = f.Bold("hello!");
+
+            Assert.Equal("*hello\\!*", hello);
+        }
+
+
+        [Fact]
+        public void Escapes_1()
+        {
+            var f = new TelegramFormatter();
+
+            var hello = f.Bold(".hello\\.");
+
+            Assert.Equal("*\\.hello\\.*", hello);
+        }
+    }
+}
